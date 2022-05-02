24f56045b85dcb1ed84dfcd7060334234121a020
Thomas <24thomasl@students.harker.org>
2022-05-01 17:18:08 -0700

    added death

--- build/game.js
+++ build/game.js
@@ -11,7 +11,8 @@ var game = {
         INTRO: me.state.USER + 0,
         ROOM: me.state.USER + 1,
         WORK: me.state.USER + 2,
-        SHOP: me.state.USER + 3
+        SHOP: me.state.USER + 3,
+        DEAD: me.state.USER + 4
     },
 
     roomText: {
@@ -63,6 +64,7 @@ var game = {
         me.state.set(game.data.INTRO, new game.OpeningScreen());
         me.state.set(game.data.ROOM, new game.RoomScreen());
         me.state.set(game.data.SHOP, new game.ShopScreen());
+        me.state.set(game.data.DEAD, new game.GameOver());
         me.pool.register("animated_people", game.WalkingPeople);
         me.pool.register("start_button", game.StartButton);
         me.pool.register("work_button", game.WorkButton);
@@ -76,6 +78,54 @@ var game = {
         me.state.change(me.state.MENU);
     }
 };
+
+game.GameOver = me.ScreenObject.extend({
+
+    init: function () {
+        this.transition = null;
+        this.exposition = null;
+    },
+
+    onResetEvent: function () {
+
+        this.transition = new me.Sprite(
+            me.video.renderer.getWidth() + 100,
+            me.video.renderer.getHeight() / 2,
+            { image: "transition" }
+        )
+        me.game.world.addChild(this.transition);
+        me.pool.pull("me.Tween", this.transition.pos).to({ x: -100 }, 500).easing(me.Tween.Easing.Linear.None).start();
+
+        this.exposition = new (me.Renderable.extend({
+
+            init: function () {
+                this._super(me.Renderable, "init", [0, 0, me.game.viewport.width, me.game.viewport.height]);
+                this.text = "";
+                this.text = this.text + "IN THE END, YOU WERE DISCOVERED BY THE THOUGHT POLICE\nAND SENT TO THE MINISTRY OF LOVE\n\n\n";
+                this.text = this.text + "AFTER BEING TORTURED, YOU FINALLY GROW TO LOVE BIG BROTHER\n\n\n";
+                this.text = this.text + "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\nGAME OVER, THANKS FOR PLAYING";
+                this.font = new me.Font("Ariel", 13, "#FFFFFF");
+                this.ypos = 600;
+                this.scroller = new me.Tween(this).to({ ypos: 20}, 5000).start();
+            },
+
+            draw: function (renderer) {
+                renderer.clearColor("#000000", true);
+                var measure = this.font.measureText(renderer, this.text);
+                this.font.draw(renderer, this.text, me.game.viewport.width/2 - measure.width/2, this.ypos);
+            }
+        }));
+
+        me.game.world.addChild(this.exposition);
+    },
+
+    onDestroyEvent: function () {
+        me.game.world.removeChild(this.transition);
+        me.game.world.removeChild(this.exposition);
+        this.transition = null;
+        this.exposition = null;
+    }
+});
 
 game.Book = me.GUI_Object.extend({
     init: function (a, b) {
@@ -338,6 +388,7 @@ game.WorkButton = me.GUI_Object.extend({
     },
 
     onClick: function (event) {
+        if(game.data.score >= 75) return true;
         me.state.change(game.data.WORK);
         return true;
     },
@@ -365,6 +416,7 @@ game.ShopButton = me.GUI_Object.extend({
     },
 
     onClick: function (event) {
+        if(game.data.score >= 75) return true;
         me.state.change(game.data.SHOP);
         return true;
     },
@@ -410,12 +462,19 @@ game.RoomScreen = me.ScreenObject.extend({
                 var measure = this.font.measureText(renderer, text);
                 this.font.draw(renderer, text, me.game.viewport.width - measure.width - 10, 20);
                 //measure = this.font.measureText(renderer, game.roomTexts.texts[game.data.roomIndex]);
-                this.font.draw(renderer, game.roomText.texts[game.data.roomIndex], 20 + 20, me.game.viewport.height/2 + 100 + 20 + 20);
+                if(game.data.score >= 75){
+                    this.font.draw(renderer, "YOU ARE THE DEAD", 20 + 20, me.game.viewport.height/2 + 100 + 20 + 20);
+                } else {
+                    this.font.draw(renderer, game.roomText.texts[game.data.roomIndex], 20 + 20, me.game.viewport.height/2 + 100 + 20 + 20);
+                }
             },
             
             update: function(x) {
                 this.cur += x;
-                if(this.cur >= 5000){
+                if(this.cur >= 3000){
+                    if(game.data.score >= 75){
+                        me.state.change(game.data.DEAD);
+                    }
                     this.cur = 0;
                     game.data.roomIndex++;
                     game.data.roomIndex %= game.roomText.texts.length;
@@ -446,13 +505,9 @@ game.RoomScreen = me.ScreenObject.extend({
         me.game.world.addChild(this.shop);
 
         me.input.bindKey(me.input.KEY.ENTER, "enter", true);
-        me.input.bindKey(me.input.KEY.SHIFT, "shift", true);
         this.handler = me.event.subscribe(me.event.KEYDOWN, function (action, keyCode, edge) {
             if(action == "enter"){
                 game.data.money++;
-            }
-            if(action == "shift"){
-                game.data.score++;
             }
         });
     },
@@ -460,7 +515,6 @@ game.RoomScreen = me.ScreenObject.extend({
     onDestroyEvent: function() {
         me.event.unsubscribe(this.handler);
         me.input.unbindKey(me.input.KEY.ENTER); 
-        me.input.unbindKey(me.input.KEY.SHIFT); 
         me.game.world.removeChild(this.telescreen);
         me.game.world.removeChild(this.shop);
         me.game.world.removeChild(this.background);
@@ -510,7 +564,7 @@ game.OpeningScreen = me.ScreenObject.extend({
                 this.text = this.text + "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\nPress ENTER to continue...";
                 this.font = new me.Font("Ariel", 13, "#FFFFFF");
                 this.ypos = 600;
-                this.scroller = new me.Tween(this).to({ ypos: 20}, 1000).start();
+                this.scroller = new me.Tween(this).to({ ypos: 20}, 5000).start();
             },
 
             draw: function (renderer) {
